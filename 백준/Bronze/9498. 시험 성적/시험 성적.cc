822439cc7bdc182e1ca51bfd3035d2173de246e6
jhc2611 <80817762+jhc2611@users.noreply.github.com>
2024-12-26 18:36:11 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    if (a<60)
+        cout << "F";
+    else if (a<70)
+        cout << "D";
+    else if (a<80)
+        cout << "C";
+    else if (a<90)
+        cout << "B";
+    else
+        cout << "A";
+}
