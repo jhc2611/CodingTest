39433524f9d08f2cf1fa730c102da193004dd7d2
jhc2611 <80817762+jhc2611@users.noreply.github.com>
2024-12-26 19:17:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, count, score;
+    cin >> a >> b >> c;
+    if (a == b&&a == c)
+        cout << 10000 + a*1000;
+    else if (a==b||a==c)
+        cout << 1000 + a*100;
+    else if (b == c)
+        cout << 1000 + b*100;
+    else
+        cout << max(max(a,b),c)*100;
+}
