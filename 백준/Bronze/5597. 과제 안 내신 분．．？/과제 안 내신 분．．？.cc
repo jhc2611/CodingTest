828309202a4a08581ce4b2c3f16216a88c840104
jhc2611 <80817762+jhc2611@users.noreply.github.com>
2025-01-01 19:58:17 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[30] = {0,};
+    for (int i=0;i<28;i++){
+        cin >> n;
+        arr[n-1] = 1;
+    }
+    for (int i=0;i<30;i++)
+        if (arr[i] == 0) cout << i+1 << endl;
+    return 0;
+}
